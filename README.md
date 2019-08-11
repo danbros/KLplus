80275bc213db37ae20fa3c466e0b27594879795f
danbros <dpb4fun@gmail.com>
2019-08-11 02:43:01 -0300

    Adicionando script principal e pyxhook, requirements.txt e setup.py.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Plus_KLL
-Um Keylogger simples para OS Linux
+# KLplus
+
+[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-green.svg)](/LICENSE)&nbsp; &nbsp;
+[![Python 3.6](https://img.shields.io/badge/Python-3.6+-blue.svg)](https://www.python.org/downloads/release/python-360/)
+
+O KLplus é um Keylogger simples para OS Linux.
+
+
+### Autoria e contribuições:
+
+[**pyxhook.py**](/pyxhook)  
+* Uma implementação do [**pyHook**](https://pypi.org/project/pyHook/) (Windows) para sistemas Linux.  Clonado do repositório de [JeffHoogland](https://github.com/JeffHoogland/pyxhook), é um pedaço do código fonte de [**Simple Python Keylogger**](https://sourceforge.net/projects/pykeylogger/), sob licença GPL v2.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from os.path import join, dirname
+
+def func_read(fname):
+    with open(join(dirname(__file__), fname)) as f:
+        return f.read()
+
+setup(
+    name='KLplus',
+    python_requires='>=3.6',
+    version='0.1',
+    url='https://github.com/danbros/Plus_KLL.git',
+    author='Dan Barros',
+    author_email='dpb4fun@gmail.com',
+    description=('A simple Keylogger for OS Linux'),
+    long_description=func_read('README.md'),
+    license='GPLv2',
+    packages=['KLplus'],
+    install_requires=['xlib'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: X11 Applications',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: System :: Monitoring',
+        'Topic :: System :: Logging',
+    ]
+)
