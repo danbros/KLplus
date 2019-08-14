e53e2c1348b471aad16a44c044b5a8316fba40b0
danbros <dpb4fun@gmail.com>
2019-08-13 23:48:15 -0300

    Adicionar arquivo de metadados (__about__) e __main__
    Por quê: O arquivo __about__ vai guardar muitos metadados
    do pacote, como author, nome, e o dado sobre a versão atual
    do pacote, deixando o máximo de informação em um só lugar.
    
    O arquivo __main__ guardará uma chamada para o klplus, para
    permitir o pacote se comportar como um script executável.
    
    Como: "python -m KLplus" chamará o método principal d klplus.py
    
    Tag: __main__, __about__

---
+++ KLplus/__about__.py
@@ -0,0 +1,13 @@
+# Arquivo para ser usado em setup
+
+__title__ = 'KLplus'
+__summary__ = 'Um Keylogger CLI para OS Linux baseado no principio KISS.'
+__uri__ = 'https://github.com/danbros/KLplus'
+
+__version__ = "0.1.1"
+
+__author__ = 'Dan Barros'
+__email__ = 'dpb4fun@gmail.com'
+
+__license__ = "GNU GPLv2"
+__copyright__ = f"Copyright 2019 {__author__}"
